bc2bc6ea7540fb003fe8cf4009631bfee0f63163
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-02-11 23:17:41 +0300

    added polygon and steps

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import plotter
 print('size \t\t    M \t\t    D')
 for size in [10, 100, 1000, 10000]:
     rands = ru.get_rand_list(size)
-    specs = ru.get_specs(rands)
+    if size == 10000:
+        specs = ru.get_specs(rands, full=True)
+    else:
+        specs = ru.get_specs(rands)
     print(size, '\t\t', round(specs['expected value'], 5), '\t', round(specs['dispersion'], 5))
 plotter.plot_autocorrelation(specs['autocorrelation'], save=True)
--- rand_utils.py
+++ rand_utils.py
@@ -25,14 +25,36 @@ def _get_autocorrelation(randoms: np.array, size: int, expected: float, dispersi
     return autocorrelation
 
 
-def get_specs(randoms: np.ndarray) -> dict:
+def _get_polygon(randoms: np.array, size: int) -> np.array:
+    min_val = min(randoms)
+    max_val = max(randoms)
+    step = (max_val - min_val) / 12
+    rel_freqs = np.zeros(12)
+    for val in randoms:
+        rel_freqs[(val-min_val) // step] += 1
+    rel_freqs = rel_freqs / size
+    return rel_freqs
+
+
+def _get_steps(polygon: np.array) -> np.array:
+    steps = np.zeros(13)
+    for i in range(len(polygon)-1):
+        steps[1+i] = sum(polygon[:i+1])
+
+
+def get_specs(randoms: np.ndarray, full=False) -> dict:
     size = len(randoms)
     expected_val = _get_expected_value(randoms, size)
     specs = {'expected value': expected_val}
     dispersion = _get_dispersion(randoms, size, expected_val)
     specs['dispersion'] = dispersion
-    autocorrelation = _get_autocorrelation(randoms, size, expected_val, dispersion)
-    specs['autocorrelation'] = autocorrelation
+    if full:
+        autocorrelation = _get_autocorrelation(randoms, size, expected_val, dispersion)
+        specs['autocorrelation'] = autocorrelation
+        polygon = _get_polygon(randoms, size)
+        specs['polygon'] = polygon
+        steps = _get_steps(polygon)
+        specs['steps'] = steps
     return specs
 
 

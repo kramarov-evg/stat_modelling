7b9095ed72c2508bb9964a625e889b7abe62d2f1
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-03-10 11:43:16 +0300

    type casting fixed for IRNUNI

--- lab2/LhW.py
+++ lab2/LhW.py
@@ -7,13 +7,14 @@ def IRNUNI(ILOW: int, IUP: int, size=1):
     uniformly distributed in interval ILOW <= IR <= IUP
 
     If size is specified,
-    returns array of IRs with size and same restrictions
+    returns array of IRs with given size and same restrictions
     '''
     if size == 1:
         u = np.random.uniform()
+        return int((IUP - ILOW + 1) * u + ILOW)
     else:
         u = np.random.uniform(size=size)
-    return int((IUP - ILOW + 1) * u + ILOW)
+        return u.astype(int)
 
 
 def IRNBIN():

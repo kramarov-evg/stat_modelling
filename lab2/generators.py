adb6e3ff46d9a27d52a0db419105c8acc01adc53
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-05-20 01:46:26 +0300

    fixed binomial generation

--- lab2/generators.py
+++ lab2/generators.py
@@ -26,22 +26,20 @@ def binomial_int(N: int, p: float, size=1):
     If size is specified,
     returns array of IRs with given size and same restrictions
     '''
-    prob_limits = np.zeros(N+1)
-    prob_limits[0] = (1 - p)**N
-    for i in range(1, N+1):
-        prob_limits[i] = prob_limits[i-1] * ((N - i + 1) / (i)) * (p / (1 - p))
-
     prob_values = np.array([np.random.uniform() for i in range(size)])
     values = np.zeros(size, dtype=np.int8)
 
     for i in range(size):
         value = 0
-        while (value < N and prob_values[i] >= 0):
-            prob_values[i] -= prob_limits[value]
+        p_cur = (1 - p)**N
+        prob_values[i] -= p_cur
+        while (prob_values[i] >= 0):
+            p_cur *= (N - value) / (value + 1) * p / (1 - p)
             value += 1
+            prob_values[i] -= p_cur
         values[i] = value
 
-    return (values if size > 1 else values[0])
+    return values if size > 1 else values[0]
 
 
 def geom_int(p: float, size=1, algo=1):
@@ -162,7 +160,7 @@ def poisson_int_accum_advanced(mu: float, size: int) -> np.ndarray:
 
 
 if __name__ == "__main__":
-    print(uniform_int(0, 9, size=10))
+    print(uniform_int(0, 10, size=10))
     print(binomial_int(10, 0.5, size=10))
     print(geom_int(0.5, size=10, algo=1))
     print(geom_int(0.5, size=10, algo=2))

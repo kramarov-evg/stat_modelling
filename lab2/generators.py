646b11388b22842af18fb7981f4a70819252d8fa
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-05-19 15:42:51 +0300

    created binomial generator

--- lab2/generators.py
+++ lab2/generators.py
@@ -17,5 +17,26 @@ def uniform_int(ILOW: int, IUP: int, size=1):
         return ((IUP - ILOW + 1) * u + ILOW).astype(int)
 
 
-def binomial_int():
-    pass
+def binomial_int(N: int, p: float, size=1):
+    '''
+    Returns int value IR,
+    binomially distributed in interval ILOW <= IR <= IUP
+
+    If size is specified,
+    returns array of IRs with given size and same restrictions
+    '''
+    prob_limits = np.zeros(N+1)
+    for i in range(1, N + 1):
+        prob_limits[i] = prob_limits[i-1] * (N - i) / (i + 1) / (1 - p) * p
+
+    prob_values = np.array([np.random.uniform() for i in range(size)])
+    values = np.zeros(size, dtype=np.int8)
+
+    for i in range(size):
+        value = 0
+        while (value < N and prob_values[i] >= 0):
+            prob_values[i] -= prob_limits[value]
+            value += 1
+        values[i] = value
+
+    return (values if size > 0 else values[0])

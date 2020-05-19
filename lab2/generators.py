97eb6bd1a685e61d6603364ae2c588b025e6bc90
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-05-19 16:25:03 +0300

    fixed binomial generator

--- lab2/generators.py
+++ lab2/generators.py
@@ -26,8 +26,9 @@ def binomial_int(N: int, p: float, size=1):
     returns array of IRs with given size and same restrictions
     '''
     prob_limits = np.zeros(N+1)
-    for i in range(1, N + 1):
-        prob_limits[i] = prob_limits[i-1] * (N - i) / (i + 1) / (1 - p) * p
+    prob_limits[0] = (1 - p)**N
+    for i in range(1, N+1):
+        prob_limits[i] = prob_limits[i-1] * ((N - i + 1) / (i)) * (p / (1 - p))
 
     prob_values = np.array([np.random.uniform() for i in range(size)])
     values = np.zeros(size, dtype=np.int8)
@@ -39,4 +40,9 @@ def binomial_int(N: int, p: float, size=1):
             value += 1
         values[i] = value
 
-    return (values if size > 0 else values[0])
+    return (values if size > 1 else values[0])
+
+
+if __name__ == "__main__":
+    print(uniform_int(0, 10, size=10))
+    print(binomial_int(10, 0.5, size=10))

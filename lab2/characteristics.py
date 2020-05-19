4b836c1ce98d8e3f648409264ba19745b70a268d
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-05-20 01:46:10 +0300

    fixed characteristics extraction & cleaned up

--- lab2/characteristics.py
+++ lab2/characteristics.py
@@ -2,56 +2,27 @@ import numpy as np
 
 
 def _get_expected_value(randoms: np.array, size: int) -> float:
-    return sum(randoms) / size
+    return randoms.sum() / size
 
 
 def _get_dispersion(randoms: np.array, size: int, expected: float) -> float:
-    return sum((randoms - expected)**2) / size
+    return ((randoms - expected)**2).sum() / size
 
 
-def _get_autocorrelation(randoms: np.array, size: int, expected: float, dispersion: float) -> list:
-    denominator = dispersion * size
-    autocorrelation = []
-    for shift in range(size):
-        numerator = 0.0
-        cut_randoms = randoms[:size - shift]
-        shifted_randoms = randoms[shift:]
-        numerator = numerator + sum((cut_randoms - expected) * (shifted_randoms - expected))
-        autocorrelation.append(numerator / denominator)
-    return autocorrelation
+def _get_polygon(randoms: np.array, size: int) -> tuple:
+    return np.unique(randoms, return_counts=True)
 
 
-def _get_polygon(randoms: np.array, size: int) -> np.array:
-    min_val = max(randoms)
-    max_val = min(randoms)
-    step = (max_val - min_val) / 12
-    rel_freqs = np.zeros(12)
-    for val in randoms:
-        rel_freqs[int((val-min_val) / step)] += 1
-    rel_freqs = rel_freqs / size
-    return rel_freqs, [min_val+step*i for i in range(12)]
-
-
-def _get_steps(randoms: np.array, polygon: np.array) -> np.array:
-    steps = np.zeros(13)
-    for i in range(len(polygon)):
-        steps[1+i] = sum(polygon[:i+1])
-    return steps, [min(randoms) + (max(randoms) - min(randoms))/12*i for i in range(13)]
-
-
-def get_specs(randoms: np.ndarray, full=False) -> dict:
+def get_specs(name: str, randoms: np.ndarray, full=False) -> dict:
     size = len(randoms)
     expected_val = _get_expected_value(randoms, size)
     specs = {'expected value': expected_val}
+    specs['randoms'] = randoms
+    specs['method'] = name
     dispersion = _get_dispersion(randoms, size, expected_val)
     specs['dispersion'] = dispersion
     if full:
-        # autocorrelation = _get_autocorrelation(randoms, size, expected_val, dispersion)
-        # specs['autocorrelation'] = autocorrelation
-        polygon, borders = _get_polygon(randoms, size)
-        specs['polygon'] = polygon
-        specs['borders1'] = borders
-        steps, borders = _get_steps(polygon, randoms)
-        specs['steps'] = steps
-        specs['borders2'] = borders
+        values, counts = _get_polygon(randoms, size)
+        specs['values'] = values
+        specs['counts'] = counts
     return specs
